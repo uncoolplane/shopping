e9f5753ad38410c4bc28feae9385e27d3c788073
Becky Hall <uncoolplane@gmail.com>
2017-07-31 19:27:32 -0600

    login screen updates

--- public/views/profile/login.js
+++ public/views/profile/login.js
@@ -1,3 +1,35 @@
-angular.module('eCommerce').controller('loginController', function($scope) {
+angular.module('eCommerce').controller('loginController', function($scope, $rootScope) {
+  $scope.reset = function(form) {
+    form.$setPristine();
+    form.$setUntouched();
+    let controlNames = Object.keys(form).filter(key => key.indexOf('$') !== 0);
+    for (let name of controlNames) {
+        let control = form[name];
+        control.$setViewValue(undefined);
+    }
 
+    $scope.username = ''
+    $scope.password = ''
+  };
+  //Master login form..takes in username and password, hits database checking if user exists. if there was no user it will fire off its callback function (reset)
+  $scope.login = (username, password, cb, form) => {
+    loginSrvc.login(username, password).then(function(res) {
+      if(res.status === 200 && res.data !== 'User did not exist') {
+        $rootScope.$broadcast('login')
+        $state.go('user')
+      } else if (res.data === 'User did not exist') {
+        alert('Username and password did not match any records')
+        cb(form)
+        $scope.username = ''
+        $scope.password = ''
+      }
+    }, function(err) {
+      if(err) {
+        alert('Username and password did not match any records')
+        cb(form)
+        $scope.username = ''
+        $scope.password = ''
+      }
+    })
+  }
 })
